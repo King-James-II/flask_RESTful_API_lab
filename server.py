b46c63ee7bcc28883320271f405893eda19f0789
King-James-II <jocundjames@gmail.com>
2024-05-06 14:48:07 -0400

    added comments

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, make_response, request
+
+# Create Flask app instance
 app = Flask(__name__)
 
+# Sample data
 data = [
     {
         "id": "3b58aade-8415-49dd-88db-8d7bce14932a",
@@ -24,41 +27,8 @@ data = [
         "country": "United States",
         "avatar": "http://dummyimage.com/148x100.png/dddddd/000000",
     },
-    {
-        "id": "66c09925-589a-43b6-9a5d-d1601cf53287",
-        "first_name": "Lilla",
-        "last_name": "Aupol",
-        "graduation_year": 1985,
-        "address": "637 Carey Pass",
-        "city": "Gainesville",
-        "zip": "32627",
-        "country": "United States",
-        "avatar": "http://dummyimage.com/174x100.png/ff4444/ffffff",
-    },
-    {
-        "id": "0dd63e57-0b5f-44bc-94ae-5c1b4947cb49",
-        "first_name": "Abdel",
-        "last_name": "Duke",
-        "graduation_year": 1995,
-        "address": "2 Lake View Point",
-        "city": "Shreveport",
-        "zip": "71105",
-        "country": "United States",
-        "avatar": "http://dummyimage.com/145x100.png/dddddd/000000",
-    },
-    {
-        "id": "a3d8adba-4c20-495f-b4c4-f7de8b9cfb15",
-        "first_name": "Corby",
-        "last_name": "Tettley",
-        "graduation_year": 1984,
-        "address": "90329 Amoth Drive",
-        "city": "Boulder",
-        "zip": "80305",
-        "country": "United States",
-        "avatar": "http://dummyimage.com/198x100.png/cc0000/ffffff",
-    }
+    ...
 ]
-
 
 # Sends back implicit 200 OK response code
 @app.route("/")
@@ -70,13 +40,14 @@ def index():
 def no_content():
     return ("hello world", 204)
 
-#Send custom HTTP response and code back with the make_response() method.
+# Send custom HTTP response and code back with the make_response() method
 @app.route("/makeres")
 def makeres():
     response = make_response({"message": "Hello World"})
     response.status_code = 202
     return response
 
+# Route to get data
 @app.route("/data")
 def get_data():
     try:
@@ -87,14 +58,10 @@ def get_data():
     except NameError:
         return {"message": "Data not found"}, 404
 
+# Route for name search
 @app.route("/name_search")
 def name_search():
-    """find a person in the database
-        Returns:
-        json: person if found, with status of 200
-        404: if not found
-        422: if argument q is missing
-    """
+    """Find a person in the database."""
     query = request.args.get("q")
 
     if query == "":
@@ -106,6 +73,7 @@ def name_search():
 
     return ({"message": "Person not found"}, 404)
 
+# Route to count data
 @app.get("/count")
 def count():
     try:
@@ -114,6 +82,7 @@ def count():
     except NameError:
         return ({"message": "No data list found"}, 500)
 
+# Route to find person by UUID
 @app.route("/person/<unique_id>")
 def find_by_uuid(unique_id):
     for person in data:
@@ -121,6 +90,7 @@ def find_by_uuid(unique_id):
             return person
     return {"message": "Unable to find person with uuid {unique_id}"}, 404
 
+# Route to delete person by UUID
 @app.route("/person/<uuid>", methods=["DELETE"])
 def delete_uuid(uuid):
     # Check if the person with the specified UUID exists in the data list
@@ -132,6 +102,7 @@ def delete_uuid(uuid):
     # If person not found, return 404 response
     return {"message": "Person not found in data list"}, 404
 
+# Route to add new person
 @app.route("/person", methods=["POST"])
 def add_by_uuid():
     new_person = request.json
@@ -143,6 +114,7 @@ def add_by_uuid():
         return {"message": "data not defined"}, 500
     return {"message": f"{new_person['id']} was successfully created"}, 200
 
+# Error handler for 404 Not Found
 @app.errorhandler(404)
 def api_not_found(error):
     return{"message": "API not found."}, 404
